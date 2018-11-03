235906027e895aad999105c74eb107fe8a510c64
Rezaul-Karim-Sujon <44495035+Rezaul-Karim-Sujon@users.noreply.github.com>
2018-11-04 02:59:46 +0600

    Add files via upload

---
+++ C#/1001/1001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            a += b;
+            Console.WriteLine("X = "+a);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/1036/1036/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1036
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c,x,y;
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            //a = Math.Sqrt(a);
+            System.Console.WriteLine(Math.Sqrt(a));
+            Console.ReadKey();
+        }
+    }
+}
